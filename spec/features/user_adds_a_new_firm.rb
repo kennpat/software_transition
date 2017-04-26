5e8054706fe82210cdec7171dea00e1ccf660282
kennpat <kennedypr@gmail.com>
2017-04-26 16:22:27 -0400

    Added Rspec

---
+++ spec/features/user_adds_a_new_firm.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'A user wants to add a new firm to convert', %{
+  As a user
+  I want to be able to add a new firm
+  So that I can track the changes in clients from one software package to another
+} do
+
+  scenario 'A user adds a new firm to the list' do
+    visit new_firm_path
+    fill_in 'Firm Name', with: 'Dewey, Cheetum & Howe, LLP'
+    click_on 'Submit'
+
+    expect(page).to have_content('Firm has been successfully added')
+  end
+end
