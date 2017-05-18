df2cef73cd51793d3680360123393bb526447110
kennpat <kennedypr@gmail.com>
2017-05-18 19:48:28 -0400

    Updated gems

---
+++ spec/features/a_user_wants_to_be_able_to_add_a_list_of_clients.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'A user wants to add a new firm to convert', %{
+  As a user
+  I want to be able to add a list of clients
+  So that I can begin tracking the transition from one software to another
+} do
+
+  scenario 'A user wants to add a list of clients to a firm' do
+
+  end
+
+end
